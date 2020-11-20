7e7370e5ec74409464396ab276f38dfe6b3af203
PatrickHaussmann <36867676+PatrickHaussmann@users.noreply.github.com>
2020-11-20 15:08:06 +0100

    create subtitle only when needed

--- class.js
+++ class.js
@@ -1,10 +1,11 @@
 function List(title, subtitle) {
     this.el = el(".list-container",
-        this.header = el("h2.header", this.title = title),
-        this.subheader = el("h3.subheader", this.subtitle = subtitle),
+        this.header = el("h2.title", this.title = title),
         this.list = el(".list"),
         this.edit = el(".item.edit", { contenteditable: "true" })
     )
+
+    if (subtitle) mount(this.el, this.subheader = el("h3.subtitle", this.subtitle = subtitle), this.list)
 
     new Sortable(this.list, {
         group: 'shared',
