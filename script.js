07b08755a90ac91b09cac2505334d7e1a8ce24e4
PatrickHaussmann <36867676+PatrickHaussmann@users.noreply.github.com>
2020-11-20 10:03:51 +0100

    testing script

---
+++ script.js
@@ -0,0 +1,39 @@
+var el = redom.el
+var mount = redom.mount
+var text = redom.text
+dayjs.locale('de')
+
+
+function stripWhitespace(string) {
+    return string.replace(/\s+/g, " ").trim()
+}
+
+
+
+
+
+var date_lists = document.getElementById("date-lists")
+var someday_lists = document.getElementById("someday-lists")
+
+
+var now = dayjs()
+let date = now.add(-2, 'day')
+for (let index = 0; index < 5; index++) {
+    date = date.add(1, 'day')
+    let list = new List(date.format("dddd"), date.format("D. MMMM YYYY"))
+    mount(date_lists, list)
+
+    let max = Math.random() * 10
+    for (let i = 0; i < max; i++) {
+        list.add(i)
+    }
+
+}
+
+date = date.add(1, 'day')
+let list = new List("Someday")
+mount(someday_lists, list)
+
+for (let i = 0; i < 10; i++) {
+    list.add(i)
+}
