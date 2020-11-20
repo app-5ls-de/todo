0e33a1d45018db9830d8cdd0cce6a4c1eef693c4
PatrickHaussmann <36867676+PatrickHaussmann@users.noreply.github.com>
2020-11-20 10:03:35 +0100

    class definition

---
+++ class.js
@@ -0,0 +1,29 @@
+function List(title, subtitle) {
+    this.el = el(".list-container",
+        this.header = el("h2.header", this.title = title),
+        this.subheader = el("h3.subheader", this.subtitle = subtitle),
+        this.list = el(".list"),
+        this.edit = el(".item.edit", { contenteditable: "true" })
+    )
+
+    new Sortable(this.list, {
+        group: 'shared',
+        filter: '.edit',
+        animation: 150
+    })
+
+    this.childs = []
+}
+
+List.prototype.add = function (text) {
+    let child = el(".item", text)
+    this.childs.push(child)
+    mount(this.list, child)
+}
+
+
+function Item(text) {
+    this.text = text
+    this.el = el(".item", text)
+}
+
