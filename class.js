e03da8ebfcfafe3441c070b4401218c3bfcbedb4
PatrickHaussmann <36867676+PatrickHaussmann@users.noreply.github.com>
2021-01-19 15:39:58 +0100

    createTodo function

--- class.js
+++ class.js
@@ -47,5 +47,91 @@ Item.prototype.click = function (self,e) {
 
 
 
-}
 
+
+
+
+function createTodo(array, id) {
+    let div_todo = el(".todo#" + id)
+    let rawText = []
+    for (let i = 0; i < array.length; i++) {
+        const element = array[i];
+        if (typeof element == "string") {
+            rawText.push(element)
+        } else {
+            if (rawText.length) {
+                mount(div_todo, el("span", rawText.join(' ')))
+                div_todo.append(" ")
+                rawText = []
+            }
+
+            let type = Object.keys(element)[0]
+
+            switch (type) {
+                case "checked": mount(div_todo, el("span.x", "x"));
+                    div_todo.classList.add("checked"); break;
+                case "priority": mount(div_todo, el("span.priority.punctuation", "("));
+                    mount(div_todo, el("span.priority", element[type]));
+                    mount(div_todo, el("span.priority.punctuation", ")")); break;
+                case "completionDate": mount(div_todo, el("span.date", element[type])); break;
+                case "creationDate": mount(div_todo, el("span.date", element[type])); break;
+                case "project": mount(div_todo, el("span.project.punctuation", "+"));
+                    mount(div_todo, el("span.project", element[type])); break;
+                case "context": mount(div_todo, el("span.context.punctuation", "@"));
+                    mount(div_todo, el("span.context", element[type])); break;
+                case "keyvalue": mount(div_todo, el("span.keyvalue.key", element[type].key));
+                    mount(div_todo, el("span.keyvalue.punctuation", ":"));
+                    mount(div_todo, el("span.keyvalue.value", element[type].value));
+            }
+        }
+        div_todo.append(" ")
+    }
+    if (rawText.length) {
+        mount(div_todo, el("span", rawText.join(' ')))
+        div_todo.append(" ")
+        rawText = []
+    }
+
+    div_todo.addEventListener('dblclick', function (e) {
+        e.preventDefault()
+
+        if (this.getAttribute("contenteditable") == "true") return
+        this.classList.add("edit")
+
+
+        this.setAttribute("contenteditable", "true")
+        this.focus()
+
+        this.addEventListener('keydown', function (e) {
+            if ((!e.shiftKey && e.key == "Enter") || e.key == "Escape") {
+                this.blur()
+                return false
+            }
+        })
+
+        let oldContent = this.innerText
+        let newContent = this.innerText
+
+        this.addEventListener('input', function (e) {
+            newContent = this.innerText
+        })
+
+        this.addEventListener('blur', function (e) {
+            this.setAttribute("contenteditable", "false")
+            this.classList.remove("edit")
+            newContent = stripWhitespace(newContent)
+            if (newContent) {
+                if (newContent != oldContent) {
+                    setRemoteData('PUT', newContent, id)
+                    addTodo(newContent, id)
+                    showTodos()
+                }
+            } else {
+                setRemoteData('DELETE', "", id)
+                addTodo("", id)
+                showTodos()
+            }
+        })
+    })
+    return div_todo
+}
